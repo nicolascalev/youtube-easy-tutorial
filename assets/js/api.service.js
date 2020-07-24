c18b346dd577e8ccfee2fe37ced91679ef4e1429
Nicolas Guillen Chaves <guillen_nicolas@live.com>
2020-07-24 00:19:46 -0600

    api service, y cambios en index en colores

---
+++ assets/js/api.service.js
@@ -0,0 +1,59 @@
+var api = axios.create({
+    baseURL = 'http://localhost:1337'
+})
+
+axios.interceptors.response.use(res => {
+    return res;
+}, err => {
+    var error;
+    if (err.response) {
+        if (err.response.data.details) {
+            error = err.response.data.details;
+        } else {
+            error = err.response.data;
+        }
+    } else {
+        error = err.message;
+    }
+    return Promise.reject(error);
+});
+
+var req = {
+
+    async findOne(model, id) {
+        try {
+            var { data } = await api.get(`/${model}/${id}`);
+            return data;
+        } catch (err) {
+            alert(err);
+        }
+    },
+
+    async find(model, params = {}, where = {}) {
+        try {
+            params.where = JSON.stringify(where);
+            var { data } = await api.get(`/${model}`, { params });
+            return data;
+        } catch (err) {
+            alert(err);
+        }
+    },
+
+    async create(model, formData) {
+        try {
+            var { data } = await api.post(`/${model}`, formData);
+            return data;
+        } catch (err) {
+            alert(err);
+        }
+    },
+
+    async destroy(model, id) {
+        try {
+            var { data } = await api.delete(`/${model}/${id}`);
+            return data;
+        } catch (err) {
+            alert(err);
+        }
+    }
+}
--- assets/js/index.page.js
+++ assets/js/index.page.js
@@ -1,4 +1,9 @@
 var app = new Vue({
     el: '#app',
-    data: {}
+    data: {
+        users: 1900,
+        subscribes: 1337,
+        downloads: 2968,
+        products: 2227,
+    }
 })
